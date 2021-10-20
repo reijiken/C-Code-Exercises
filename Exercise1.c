c0b1b435786910394c57704480d415fc61a205ac
Nguyen Quang Huy <78330758+Dev789-del@users.noreply.github.com>
2021-10-20 14:55:41 +0700

    Create Exercise1.c

---
+++ Exercise1.c
@@ -0,0 +1,10 @@
+/* C program to print the double value of input number */
+#include<stdio.h>
+int main()
+{
+  int A;
+  printf("Enter number A: ");
+  scanf("%d",&A);
+  printf("%d",2*A);
+  return 0;
+}
