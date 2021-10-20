8fa1bbe022374c681511498347aea3f3d9226365
Nguyen Quang Huy <78330758+Dev789-del@users.noreply.github.com>
2021-10-20 20:47:36 +0700

    Create Exercise2.c

---
+++ Exercise2.c
@@ -0,0 +1,11 @@
+// C code print the result of (A-B)*C
+#include<stdio.h>
+int main()
+{
+  int A;
+  int B;
+  int C;
+  printf("Enter three numbers : ");
+  scanf("%d %d %d",&A,&B,&C);
+  printf("Formula = %d",((A-B)*C)));
+}
