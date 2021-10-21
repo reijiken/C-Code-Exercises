56b2b8341c7d6339fc734c45c5fa4a047a624ecf
Nguyen Quang Huy <78330758+Dev789-del@users.noreply.github.com>
2021-10-21 08:30:55 +0700

    Create Reverse.c

---
+++ Reverse.c
@@ -0,0 +1,19 @@
+// C program to output the reverse of input string
+#include<stdio.h>
+int main()
+{
+  char s[1000], r[1000];
+  int begin, end, count = 0;
+  printf("Enter a string: \n");
+  gets(s);
+  while(s[count] != '\0')
+    count++;
+  end = count - 1;
+  for(begin = 0; begin < count; begin++){
+    r[begin] = s[end];
+    end--;
+  }
+  r[begin] = '\0';
+  printf("%s\n",r);
+  return 0;  
+}
