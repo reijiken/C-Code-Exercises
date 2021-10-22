da8d895c8257db7b0155331ce4e34abf1bcaaabe
Nguyen Quang Huy <78330758+Dev789-del@users.noreply.github.com>
2021-10-22 13:24:12 +0700

    Create Factorial.c

---
+++ Factorial.c
@@ -0,0 +1,17 @@
+// C program to compute the factorial of N using recursion
+#include <stdio.h>
+#include <math.h>
+int factorial(int n)
+{
+    if(n == 0)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}    
+int main() {
+    int N;
+    printf("Enter a number: ");
+    scanf("%d",&N);
+    printf("Factorial of input number: %d",factorial(N));
+    return 0;
+}
