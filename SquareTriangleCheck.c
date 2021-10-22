b30447cb4225ffb18c27670cc015ae0c53a6a4ec
Nguyen Quang Huy <78330758+Dev789-del@users.noreply.github.com>
2021-10-22 21:35:40 +0700

    Create SquareTriangleCheck.c

---
+++ SquareTriangleCheck.c
@@ -0,0 +1,13 @@
+// C program to check if three input numbers are parts of a square triangle
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d",&a,&b,&c);
+    if(a*a + b*b == c*c || a*a + c*c == b*b || b*b + c*c == a*a)
+        printf("YES");
+    else 
+        printf("NO");
+    return 0;
+}
