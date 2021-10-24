92790a45e0bfaeefd4b3cf0bd56112b368375945
Nguyen Quang Huy <78330758+Dev789-del@users.noreply.github.com>
2021-10-24 11:05:55 +0700

    Create RandomRectangle.c

---
+++ RandomRectangle.c
@@ -0,0 +1,23 @@
+// English: C program to print a rectangle of # with input two numbers
+// Vietnamese: Chương trình tạo hình chữ nhật thăng
+#include<stdio.h>
+int main()
+{
+    int i, j, N, M;
+    printf("Enter two numbers: ");
+    scanf("%d %d",&N,&M);
+    if (N < 0 || M < 0)
+    {
+        printf("Invalid numbers");
+    }
+    else
+    {
+        for (i = 1; i <= M; i++)
+        {
+            for (j = 1; j <= N; j++)
+                printf("#");
+            printf("\n");   
+        }   
+    }
+    return 0;
+}
