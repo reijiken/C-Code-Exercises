dee1482c1e10a3b7181c61bd52d412edf9184e56
Nguyen Quang Huy <78330758+Dev789-del@users.noreply.github.com>
2021-10-25 09:22:53 +0700

    Create DoubleString.c

---
+++ String/DoubleString.c
@@ -0,0 +1,10 @@
+//C program print two same string as you input a string
+#include <stdio.h>
+
+int main() {
+    char s[1000];
+    printf("Enter a string: ");
+    gets(s);
+    printf("%s %s",&s,&s);
+    return 0;
+} 
