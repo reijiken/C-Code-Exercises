edc0f46f9f9a7bd62c5e33881381b67040578207
Nguyen Quang Huy <78330758+Dev789-del@users.noreply.github.com>
2021-10-26 10:30:21 +0700

    Create CubedNumber.c

---
+++ NumberWorld/CubedNumber.c
@@ -0,0 +1,11 @@
+// C program to print the cubed value of input number
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    float a;
+    printf("Enter a number: ");
+    scanf("%f",&a);
+    printf("Square of a number: %f ",a*a*a);
+    return 0;
+}
