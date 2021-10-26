168d93cf2130e405aadd597f078d4ca4c7c1c4c7
Nguyen Quang Huy <78330758+Dev789-del@users.noreply.github.com>
2021-10-26 10:29:10 +0700

    Create SquareNumber.c

---
+++ NumberWorld/SquareNumber.c
@@ -0,0 +1,11 @@
+// C program to print the square value of input number
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    float a;
+    printf("Enter a number: ");
+    scanf("%f",&a);
+    printf("Square of a number: %.2f ",a*a);
+    return 0;
+}
