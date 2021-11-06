5b5cec904bb1024d7a703e5f6d5d7819a9a7c904
Nguyen Quang Huy <78330758+Dev789-del@users.noreply.github.com>
2021-11-06 14:06:53 +0700

    Create BasicCounting.c

---
+++ NumberWorld/BasicCounting.c
@@ -0,0 +1,14 @@
+// C program to calculate the basic counting between A and B
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int A, B;
+    printf("Enter two numbers: ");
+    scanf("%d %d",&A,&B);
+    printf("A + B = %d",(A+B));
+    printf("\nA - B = %d",(A-B));
+    printf("\nA * B = %d",(A*B));
+    printf("\nA / B = %d",(A/B));
+    return 0;
+}
