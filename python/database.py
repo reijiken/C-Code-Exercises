4419e4e4dc73cb70ef5a8ffea193e3ce1546e0b1
Dương <92847307+reijiken@users.noreply.github.com>
2022-04-24 12:33:52 +0700

    update

--- python/database.py
+++ python/database.py
@@ -9,12 +9,27 @@ c = conn.cursor()
 
 c.execute('''
           CREATE TABLE IF NOT EXISTS products
-          ([product_id] INTEGER PRIMARY KEY, [product_price] INTEGER, [product_quantity] INTEGER, [product_size] TEXT)
+          ([product_id] INTEGER PRIMARY KEY, [product_name] TEXT, [product_price] INTEGER, [product_quantity] INTEGER)
           ''')
           
 c.execute('''
           CREATE TABLE IF NOT EXISTS safety_box
-          ([tiền_mua_hàng] INTEGER , [tiền_bán] INTEGER, [tiền_duy_trì_quán] INTEGER)
+          ([tiền_mua_hàng] INTEGER, [tiền_bán] INTEGER, [tiền_duy_trì_quán] INTEGER)
+          ''')
+
+c.execute('''
+          CREATE TABLE IF NOT EXISTS customer
+          ([name] TEXT, [age] INTEGER, [phone number] INTEGER)
+          ''')
+
+c.execute('''
+          CREATE TABLE IF NOT EXISTS staff
+          ([name] TEXT, [age] INTEGER, [phone number] INTEGER, [salary] INTEGER , [shift] DATETIME)
+          ''')
+
+c.execute('''
+          CREATE TABLE IF NOT EXISTS order
+          ([order number] INTEGER , [order date] DATETIME, [order status] INTEGER)
           ''')
                      
 conn.commit()
@@ -25,19 +40,17 @@ conn = sqlite3.connect('test_database')
 c = conn.cursor()
                    
 c.execute('''
-          INSERT INTO products (product_id, product_price, product_quantity,product_size)
-
+          INSERT INTO products (product_id, product_name, product_price, product_quantity)
                 VALUES
-                (1,'Computer'),
-                (2,'Printer'),
-                (3,'Tablet'),
-                (4,'Desk'),
-                (5,'Chair')
+                (1,'pants',10,100),
+                (2,'T-shirt',7,100),
+                (3,'socks',1,100),
+                (4,'shoes',20,100),
+                (5,'skirt',11,100)
           ''')
 
 c.execute('''
           INSERT INTO safety_box (tiền_mua_hàng, tiền_bán, tiền_duy_trì_quán)
-
                 VALUES
                 (1,800),
                 (2,200),
@@ -46,6 +59,29 @@ c.execute('''
                 (5,150)
           ''')
 
+c.execute('''
+          INSERT INTO customer (name, age, phone number)
+                VALUES
+                ('A',18,01246384345),
+                ('B',21,07286528568),
+                ('C',20,06523852698)
+          ''')
+
+c.execute('''
+          INSERT INTO staff (name, age, phone number, salary, shift)
+                VALUES
+                ('C',21,0354285146,10,'19:00:00'),
+                ('D',23,0142342244,10,'8:00:00'),
+                ('E',19,0985366686,10,'14:00:00')
+          ''')
+
+c.execute('''
+          INSERT INTO order (order number, order date, order status)
+                VALUES
+                (1,'2022-2-12 13:00:00',ongoing),
+                (2,'2022-3-15 10:00:00',ongoing),
+                (3,'2022-4-20 19:00:00',cancel)
+          ''')
 conn.commit()
 
 #Display table part
@@ -58,7 +94,10 @@ c = conn.cursor()
 c.execute('''
           SELECT
           a.products,
-          b.safetybox
+          b.safetybox,
+          c.customer,
+          d.staff,
+          e.order
           FROM products a
           LEFT JOIN safety_box b ON a.product_id = b.product_id
           ''')
